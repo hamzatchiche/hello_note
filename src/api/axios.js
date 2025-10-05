b0983b7bd72f5fb4a007f8ea7df76af72c938485
hamzatchiche <146874815+hamzatchiche@users.noreply.github.com>
2025-10-05 19:20:58 +0100

    update api

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,7 +7,7 @@ const api = axios.create({
     }
 });
 
-// Ajouter token automatiquement
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
